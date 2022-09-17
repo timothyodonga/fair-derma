c10118f786b62b84fcfb64d18101f2262fbdc8d3
timothyodonga <timothy.odonga@gmail.com>
2022-09-17 19:36:31 +0300

    Removed comments from sampler.py

--- utils/sampler.py
+++ utils/sampler.py
@@ -3,14 +3,10 @@ from torch.utils.data.sampler import SubsetRandomSampler
 
 
 def get_random_subset_sampler(len_train_data, valid_size=0.2):
-    # split the dataset in train and test set
-    # valid_size = 0.2
-    # num_train = len(train_data)
     num_train = len_train_data
 
     indices = list(range(num_train))
     split = int(np.floor(valid_size * num_train))
-    # np.random.shuffle(indices)
 
     train_idx, valid_idx = indices[split:], indices[:split]
     print(len(train_idx), len(valid_idx))
